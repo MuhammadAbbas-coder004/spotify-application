2e5a46bbf90f46b3425a6fff7ef565d88b39e856
AbuzarDev7 <zadaazadaa72@gmail.com>
2026-08-20 10:51:53 -0700

    0.5% done

---
+++ src/db/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+
+const connectDB = async() => {
+try{
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log("<'-'> DB Is Connected Successfully <'-'>");
+    
+}catch(err){
+console.log(`${err}|XXX| DB Connection Is Failed |XXX|`);
+
+}
+}
+
+
+export default connectDB;
